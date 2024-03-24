6558b895eac487b36c8b40d10985b887cddd2b63
Mukul <153187357+mukulsar143@users.noreply.github.com>
2024-03-24 18:45:34 +0530

    Some Corrections

--- Blogs/Blogs/settings.py
+++ Blogs/Blogs/settings.py
@@ -173,9 +173,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_HOST_USER = 'makessidd143@gmail.com'
+EMAIL_HOST_USER = 'youremail@gmail.com'
 
-EMAIL_HOST_PASSWORD = 'eemr pyld lyuo xekv'
+EMAIL_HOST_PASSWORD = 'your password'
 
 EMAIL_USE_TLS = True
 
